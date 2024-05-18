1b5c2c677577d36269fd7168ca89dd216fe42d59
hsthe29 <The.HS200614@sis.hust.edu.vn>
2024-05-18 12:29:11 +0700

    handle routing

--- src/main/kotlin/hust/thehs/plugins/Routing.kt
+++ src/main/kotlin/hust/thehs/plugins/Routing.kt
@@ -1,18 +1,19 @@
 package hust.thehs.plugins
 
+import com.jillesvangurp.ktsearch.deleteDocument
 import com.jillesvangurp.ktsearch.search
 import com.jillesvangurp.searchdsls.querydsl.bool
 import com.jillesvangurp.searchdsls.querydsl.match
+import com.jillesvangurp.searchdsls.querydsl.matchPhrase
 import hust.thehs.*
-import io.ktor.http.*
-import io.ktor.resources.*
+import hust.thehs.plugins.extractor.extract
+import hust.thehs.plugins.index.index
+import hust.thehs.plugins.search.search
 import io.ktor.server.application.*
-import io.ktor.server.resources.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.html.InputType
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.seconds
@@ -20,73 +21,43 @@ import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRouting() {
     val client = getClient()
-    val indexName = DEFAULT_INDEX_NAME
-    val JsonMapper = Json { ignoreUnknownKeys = true }
-
 
     install(Resources)
+
     routing {
         get("/") {
-//            call.respondText("Hello World!")
-        }
-        get<Articles> { article ->
-            // Get all articles ...
-            call.respond("List of articles sorted starting from ${article.sort}")
+            call.respondText("Plagiarism Indexing Server")
         }
 
-        get("/search") {
-            val queryParameters = call.request.queryParameters
-            val numFiles = queryParameters["numberFile"]?.toInt() ?: 0
-            val keywords = queryParameters["keyword"]?.split(",") ?: listOf()
-            val language = when(queryParameters["language"]) {
-                "vi" -> Language.VI
-                "en" -> Language.EN
-                else -> throw Exception("Language ${queryParameters["language"]} is not valid or not supported")
-            }
-            val res = when(language) {
-                Language.VI -> {
-                    client.search(target = indexName, size = numFiles, timeout = 0.5.seconds) {
-                        query = bool {
-                            must(
-                                match("language", language.lang)
-                            )
-                            should(
-                                keywords.map { match("data", it) }
-                            )
-                        }
-                    }
-                }
-                Language.EN -> {
-                    client.search(target = indexName, size = numFiles, timeout = 0.5.seconds) {
-                        query = bool {
-                            should(
-                                keywords.map { match("data", it) }
-                            )
-                        }
-                    }
-
-                }
-            }
-
-            val returnJson = RespondJson(
-                hits = res.hits?.hits?.map {
-                    JsonMapper.decodeFromString<Hit>(it.source.toString())
-                        .apply {
-                            score = it.score ?: 0.0
-                        }
-                }?.toList() ?: listOf(),
-                status = 200
-            )
-
-            call.respondText(Json.encodeToString(returnJson))
+        post("/search") {
+            val searchConfig = call.receive<SearchConfig>()
+            val respondJson = search(client, searchConfig)
+            call.respondText(Json.encodeToString(respondJson))
         }
 
         post("/index") {
+            val indexConfig = call.receive<IndexConfig>()
+            println("Index config: $indexConfig")
+            if (indexConfig.extract) {
+                extract(indexConfig.directoryPath)
+            }
+            index(indexConfig.directoryPath, indexConfig)
+            call.respondText(indexConfig.toString())
+        }
 
+        get("/remove") {
+            val queryParameters = call.request.queryParameters
+            val filePath = queryParameters["file_path"] ?: ""
+
+            val res = client.search(target = DEFAULT_INDEX_NAME) {
+                query = matchPhrase("file_path", filePath)
+            }
+
+            val hits = res.hits?.hits ?: listOf()
+            for(hit in hits) {
+                client.deleteDocument(target = "documents", id = hit.id)
+                println("Deleted document id = ${hit.id}")
+            }
         }
     }
 }
-
-@Serializable
-@Resource("/articles")
-class Articles(val sort: String? = "new")
